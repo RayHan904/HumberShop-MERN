15c79ff1839322031716ae5a2f15857713e2e969
rayhan904 <rehan904@yahoo.com>
2023-07-10 23:12:55 -0400

    Added Readme.md

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import path from 'path'
+
 
 const port = process.env.PORT || 5001;
 
